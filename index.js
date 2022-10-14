7697fe60275fa5b12aaed12a97fe55ec9e97d77d
Airell Mantilla <airell.mantz@gmail.com>
2022-10-14 00:43:20 -0400

    automation to find trees completed

--- index.js
+++ index.js
@@ -8,27 +8,24 @@ const robot = require('robotjs');
 function main(){
     console.log("Starting...");
     sleep(4000);
-    
-    var firstTree_x = 1190;
-    var firstTree_y = 510;
-    var secondTree_x = 830;
-    var secondTree_y = 475;
 
     // Loop forever until stopped manually
     while(true){
-        //chop first tree
-        robot.moveMouse(firstTree_x, firstTree_y);
+        var tree = findTree();
+        
+        //if tree isn't found, display an error and exit
+        if (tree == false){
+            console.log("No tree found");
+            break;
+        }
+
+        //chop down found tree
+        robot.moveMouse(tree.x, tree.y);
         robot.mouseClick();
         sleep(8000);
 
         dropLogs();
 
-        //chop second tree
-        robot.moveMouse(secondTree_x, secondTree_y);
-        robot.mouseClick();
-        sleep(8000);
-
-        dropLogs();
     }
 
     console.log("Done.");
@@ -46,13 +43,39 @@ function dropLogs(){
     sleep(1000);
 }
 
-// snapshot screen for coordinates of trees
-function snapshot(){
-    //taking a screenshot
-    var img = robot.screen.capture(0, 0, 1920, 1080);
 
-    var pixelColor = img.colorAt(890, 790);
-    console.log(pixelColor);
+// find a tree on the screen
+function findTree(){
+    var x = 300, y = 300, width = 1300, height = 400;
+    var img = robot.screen.capture(x, y, width, height);
+
+    var treeColours = ["705634", "60492c", "5b462a", "544026", "6d5432", "6a5130", "574328"];
+
+    for(var i = 0; i < 100; i++){ 
+        random_x = getRandomInt(0, width - 1);
+        random_y = getRandomInt(0, height - 1);
+
+        var sampleColour = img.colorAt(random_x, random_y);
+
+        if (treeColours.includes(sampleColour)){
+            var screen_x = x + random_x;
+            var screen_y = y + random_y;
+
+            console.log("Found tree at " + screen_x + ", " + screen_y + " with colour " + sampleColour);
+
+            return {x: screen_x, y: screen_y};
+        }
+    }
+
+    //couldn't find a tree in snapshot
+    return false;
+}
+
+// generate a random integer between min and max
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // pause execution for a given number of milliseconds
@@ -60,5 +83,13 @@ function sleep(ms) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 }
 
-snapshot();
-//main();
+// snapshot screen for colour of trees
+function snapshot(){
+    //taking a screenshot
+    var img = robot.screen.capture(0, 0, 1920, 1080);
+    var pixelColor = img.colorAt(890, 790);
+    console.log(pixelColor);
+}
+
+//snapshot();
+main();
