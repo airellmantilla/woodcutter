1291a3b92b6f27001a5cb1b59242def2baf2de10
Airell Mantilla <airell.mantz@gmail.com>
2022-10-13 23:05:47 -0400

    basic click function

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+// woodcutting script by airellmantilla
+// Tested on 1920x1080 resolution
+// Tested on Ikov.io client
+
 // import the robotjs library
 const robot = require('robotjs');
 
-console.log("Starting...");
-robot.moveMouse(0, 0);
-console.log("Done.");
+function main(){
+    console.log("Starting...");
+    sleep(4000);
+    
+    var i = 0;
+
+    while(i < 5){
+        robot.moveMouse(825, 550);
+        robot.mouseClick();
+        sleep(8000);
+        i++;
+    }
+
+    console.log("Done.");
+}
+
+// pause execution for a given number of milliseconds
+function sleep(ms) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+}
+
+main();
